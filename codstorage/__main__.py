84d053819e1893b902923770772816e0d1cbc923
Joao Meyer <1994meyer@gmail.com>
2022-07-12 00:54:00 +0100

    Simpler command

--- codstorage/__main__.py
+++ codstorage/__main__.py
@@ -1,15 +1,22 @@
-from pathlib import Path
-from argparse import ArgumentParser
+import sys
+from argparse import ArgumentParser, FileType
 
 from git import Repo
 
 from codstorage.ipld import IPLD
 
+FILE_OUT = FileType('wt')
+
 parser = ArgumentParser()
-parser.add_argument('path', type=Path)
+parser.add_argument('repo', type=Repo)
+parser.add_argument('-o', '--output', default=sys.stdout, type=FILE_OUT)
 parser.add_argument('-v', '--verbose', action='count')
 args = parser.parse_args()
 
-ipld = Repo(args.path)
-ipld = IPLD(ipld)
-ipld.send()
+ipld = IPLD(args.repo)
+ipld = ipld.send()
+
+output = ipld['/']
+output = f'IPLD: {output}'
+
+args.output.write(output)
