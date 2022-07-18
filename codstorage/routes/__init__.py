21e90334dd1c721f7f5832ebcb7174d38597e148
Joao Meyer <1994meyer@gmail.com>
2022-07-19 00:03:14 +0100

    Added ping

--- codstorage/routes/__init__.py
+++ codstorage/routes/__init__.py
@@ -6,6 +6,14 @@ from .repos import router as router_repos
 
 
 router = APIRouter()
+
+
+@router.get('/ping')
+async def ping():
+    return 'pong'
+
+
 router.include_router(router_ipfs, prefix='/ipfs')
 router.include_router(router_users, prefix='/users')
 router.include_router(router_repos)
+
