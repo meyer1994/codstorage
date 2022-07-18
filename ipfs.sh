99df16e509bf3755baa37f01a13dcc534a4a5efc
Joao Meyer <1994meyer@gmail.com>
2022-07-19 00:50:40 +0100

    Copying

--- ipfs.sh
+++ ipfs.sh
@@ -6,8 +6,5 @@ sleep 15
 
 ipfs config --json API.HTTPHeaders.Access-Control-Allow-Origin '["*"]'
 ipfs config --json API.HTTPHeaders.Access-Control-Allow-Methods '["GET", "PUT", "POST"]'
-ipfs config --json API.HTTPHeaders.Access-Control-Allow-Headers '["Authorization"]'
-ipfs config --json API.HTTPHeaders.Access-Control-Expose-Headers '["Location"]'
-ipfs config --json API.HTTPHeaders.Access-Control-Allow-Credentials '["true"]'
 
 ipfs log tail
