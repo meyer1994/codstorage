816a67d2b3ca96c81663e5b33bdfce9df53907f1
Joao Meyer <1994meyer@gmail.com>
2022-07-19 00:42:31 +0100

    New conf

--- ipfs.sh
+++ ipfs.sh
@@ -5,7 +5,7 @@ ipfs daemon --init &
 sleep 15
 
 ipfs config --json API.HTTPHeaders.Access-Control-Allow-Origin '["*"]'
-ipfs config --json API.HTTPHeaders.Access-Control-Allow-Methods '["GET", "POST"]'
+ipfs config --json API.HTTPHeaders.Access-Control-Allow-Methods '["GET", "PUT", POST"]'
 ipfs config --json API.HTTPHeaders.Access-Control-Allow-Headers '["Authorization"]'
 ipfs config --json API.HTTPHeaders.Access-Control-Expose-Headers '["Location"]'
 ipfs config --json API.HTTPHeaders.Access-Control-Allow-Credentials '["true"]'
