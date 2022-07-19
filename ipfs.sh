cbdc411fc8fd7e28a0058e8f6b7b79b41339682b
Joao Meyer <1994meyer@gmail.com>
2022-07-19 01:14:12 +0100

    Trying again

--- ipfs.sh
+++ ipfs.sh
@@ -7,4 +7,7 @@ sleep 15
 ipfs config --json API.HTTPHeaders.Access-Control-Allow-Origin '["*"]'
 ipfs config --json API.HTTPHeaders.Access-Control-Allow-Methods '["GET", "PUT", "POST"]'
 
+ipfs config Addresses.API '/ip4/0.0.0.0/tcp/5001'
+ipfs config Addresses.Gateway '/ip4/0.0.0.0/tcp/8080'
+
 ipfs log tail
