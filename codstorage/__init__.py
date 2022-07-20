a3d0c20f92ceaf21dfa933fe4d816dd371d6ef79
Joao Meyer <1994meyer@gmail.com>
2022-07-20 01:02:41 +0100

    Fixing config

--- codstorage/__init__.py
+++ codstorage/__init__.py
@@ -21,6 +21,10 @@ app.add_middleware(
 
 @app.on_event('startup')
 async def startup():
+    # Create
+    config.CODSTORAGE_REPOS_DIR.mkdir(parents=True, exist_ok=True)
+    config.CODSTORAGE_SQLITE_FILE.touch(exist_ok=True)
+
     await db.connect()
     query = '''
         CREATE TABLE IF NOT EXISTS repos(
@@ -30,10 +34,6 @@ async def startup():
         )
     '''
     await db.execute(query)
-
-    # Create
-    config.CODSTORAGE_REPOS_DIR.mkdir(parents=True, exist_ok=True)
-    config.CODSTORAGE_SQLITE_FILE.touch(exist_ok=True)
 
 
 @app.on_event('shutdown')
